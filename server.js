33880dc81e89c7dfb053296e1c74404ea9418521
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-25 17:54:25 -0700

    connect to jawsdb

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 // import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001; //HEROKU COMPTABILE https://coding-boot-camp.github.io/full-stack/heroku/deploy-with-heroku-and-mysql
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
