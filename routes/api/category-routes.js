4fe23fc6e8449e535b8d83c13aa5938a38e23890
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-25 18:31:05 -0700

    api category code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,11 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  Category.findAll({ include: { model: Product, attributes: ['id', 'product_name', 'price', 'stock', 'category_id']}})
+  .then(database => { if(!database) { res.status(400).json({message: 'nothing found'});
+  return;
+}res.json(database);}).catch(err => { console.log(err);res.status(500).json(err) });});
+
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
