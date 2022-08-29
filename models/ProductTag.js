e43080b2b3c9fc0af192418e18e1e02a6461da0b
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-29 08:34:51 -0700

    properties for datatypes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
-
 class ProductTag extends Model {}
 
-ProductTag.init(
-  {
-    // define columns
+ProductTag.init({ 
+    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
+
+    product_id: { type: DataTypes.INTEGER, allowNull: true, references: { model: 'product', key: 'id' } },
+
+    tag_id: { type: DataTypes.INTEGER, allowNull: true, references: { model: 'tag', key: 'id' } }
   },
   {
     sequelize,
