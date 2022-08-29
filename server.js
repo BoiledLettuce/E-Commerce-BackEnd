01e2f3467acdfa065b90e6d7582c5e5e2335c4b1
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-29 08:29:00 -0700

    fixed sequelize route added sequelize function

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
-
+const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001; //HEROKU COMPTABILE https://coding-boot-camp.github.io/full-stack/heroku/deploy-with-heroku-and-mysql
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() =>{
+
+  app.listen(PORT, () => { console.log(`App listening on port ${PORT}!`); });
+
 });
+
+// app.listen(PORT, () => { console.log(`App listening on port ${PORT}!`); });
