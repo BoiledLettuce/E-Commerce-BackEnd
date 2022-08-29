cd31ac60d5551b0d067a79e773221e5e4038a98b
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-08-29 08:32:31 -0700

    properties for category model

--- models/Category.js
+++ models/Category.js
@@ -1,23 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
-
 class Category extends Model {}
 
-Category.init(
-  {
-
-    // define columns
-    id: { type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
-
-    category_name: { type: DataTypes.STRING, allowNull: false }
-    
-  },
+Category.init({ id: { type: DataTypes.INTEGER, allowNull: false, autoIncrement: true, primaryKey: true },
+    category_name: { type: DataTypes.STRING, allowNull: false } },
   {
     sequelize,
-    timestamps: false,
     freezeTableName: true,
     underscored: true,
+    timestamps: false,
     modelName: 'category',
   }
 );
